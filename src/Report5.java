2f2667419a66155cc63d3cccaa8b6af24bff9c15
Ryuki Higa <e225759@ie.u-ryukyu.ac.jp>
2023-02-03 15:43:18 +0900

    1st commit

--- src/Report5.java
+++ src/Report5.java
@@ -1,6 +1,7 @@
 public class Report5{
     public static void main(String[] args) throws Exception {
-        String str =null;
-        System.out.println(str.length());
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
+        System.out.println(value);
     }
 }
